242f0472f06988e8eea69936e489b8ead38f3c36
justingywen <justinweiheng@gmail.com>
2026-07-07 11:13:30 +0800

    Create HighScoreCheck.java

---
+++ 0707/HighScoreCheck.java
@@ -0,0 +1,9 @@
+public class HighScoreCheck {
+    public static void main(String[] args) {
+        int highScore = 100;
+        if(highScore >= 90) {
+            System.out.println("Excellent");
+        } 
+        
+    }
+}
